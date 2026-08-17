7212b5549d220f39d3ef7a7b3f548f7e72732538
feazeved <felpssuassuna@gmail.com>
2026-08-17 22:24:32 +0100

    added basic structure dockers

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+	return (
+		<main className="min-h-screen bg-slate-950 text-white flex flex-col items-center justify-center">
+			<h1 className="text-6xl font-bold">
+				FT_TRANSCENDENCE
+			</h1>
+
+			<p className="mt-4 text-xl text-slate-400">
+				UNO
+			</p>
+		</main>
+	)
+}
+
+export default App
