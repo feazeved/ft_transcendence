e6ec39829fed95af5c17fd73058813c0f008ee4f
feazeved <felpssuassuna@gmail.com>
2026-08-17 18:37:11 +0100

    removed files from merge that I shoouldn't have done

--- game_api/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class GameApiConfig(AppConfig):
-    name = 'game_api'
--- game_api/routing.py
+++
@@ -1,3 +0,0 @@
-# WebSocket URL patterns for game_api, empty for now
-
-websocket_urlpatterns = []
