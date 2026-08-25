a155eff81c24bca6522c2279ef921be5c128c8c3
magusk89 <magusk.a.lutus@gmail.com>
2026-08-25 03:53:13 +0100

    feat: adjustments to init in game_engine to import all modules

---
+++ backend/game_engine/__init__.py
@@ -0,0 +1,12 @@
+from .cards import Card, CardType, Color
+from .deck import Deck, build_standard_deck
+from .engine import (GameOver, IllegalMove, draw_card, pass_turn, play_card, start_game)
+from .state import Direction, GameSettings, GameState, Player
+from . import modifiers
+
+__all__ = [
+	"Card", "CardType", "Color",
+	"Deck", "build_standard_deck",
+	"GameOver", "IllegalMove", "draw_card", "pass_turn", "play_card", "start_game",
+	"Direction", "GameSettings", "GameState", "Player",
+]
