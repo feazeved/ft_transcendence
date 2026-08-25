ca7163bd091946b6178cc1856b6a93f794fee1b6
Danielfonsecaa <danieldafonseca@sapo.pt>
2026-08-25 18:00:49 +0100

    add comments on the front end

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,8 @@
+// '@/...' is an alias for 'src/...', in vite.config.js and jsconfig.json, so paths don't get to big '../../routes.jsx'.
 import AppRoutes from '@/routes.jsx'
 
+// A "component" is just a function that returns JSX (HTML-like syntax in JS).
+// App is the root component
 function App() {
 	return <AppRoutes />
 }
--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,3 +1,5 @@
+// <Link> is the same as <a>. <a href="/"> make a full reload on the browser
+// <Link> updates with JS instead, avoiding the full reload.
 import { Link } from 'react-router'
 
 function NotFound() {
@@ -5,6 +7,7 @@ function NotFound() {
 		<main className="min-h-screen bg-slate-950 text-white flex flex-col items-center justify-center gap-4">
 			<h1 className="text-4xl font-bold">404</h1>
 			<p className="text-slate-400">Page not found.</p>
+			{/* "to" is Link's equivalent of href */}
 			<Link to="/" className="underline">Back home</Link>
 		</main>
 	)
