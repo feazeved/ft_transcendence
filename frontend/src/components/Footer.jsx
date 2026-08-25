87cebcc55f1310bf689e60686b7465630c4ea7d3
Danielfonsecaa <danieldafonseca@sapo.pt>
2026-08-25 22:20:36 +0100

    feature: added navbar and little footer  - also create modals for card and navbar button  - added few assets  - all the buttons do nothing

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+	<footer className=" pl-4 pr-16 bg-[#401d35] text-white">
+		<p>Made with LOVE</p>
+	</footer>
+  );
+};
+
+export default Footer;
---
+++ frontend/src/pages/Profile.jsx
@@ -0,0 +1,15 @@
+import { Link } from 'react-router'
+
+
+function Profile() {
+	return (
+		<div className="min-h-screen bg-background text-white flex items-center justify-center">
+			<h1 className="text-6xl font-bold">
+				PROFILE
+			</h1>
+			<Link to="/" className="underline">Back home</Link>
+		</div>
+	)
+}
+
+export default Profile
