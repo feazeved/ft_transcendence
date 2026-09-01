6788088d499abb5634c8ff0bf22ed48fe69b266d
magusk89 <magusk.a.lutus@gmail.com>
2026-09-01 16:18:27 +0100

    feat: prepared the core urls for authentication implementation on the game_api;

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -17,11 +17,14 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+	path('admin/', admin.site.urls),
+	path('api/auth/', include('dj_rest_auth.urls')),
+	path('api/auth/registration', include('dj_rest_auth.registration.urls')),
+	path('accounts/', include('allauth.urls')),
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+	urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
