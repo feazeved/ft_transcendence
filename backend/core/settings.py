19d98574011bec13fc35cced2df03faea33cfc79
magusk89 <magusk.a.lutus@gmail.com>
2026-09-02 14:59:18 +0100

    fix: fixed a typo in the core settings;

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -156,7 +156,7 @@ REST_FRAMEWORK = {
 	'DEFAULT_AUTHENTICATION_CLASSES': [
 		'rest_framework.authentication.SessionAuthentication',
 	],
-	'DEFAULT_PERMISSIONS_CLASSES': [
+	'DEFAULT_PERMISSION_CLASSES': [
 		'rest_framework.permissions.IsAuthenticated',
 	],
 }
